917703da637c926aaee2e3d836eb167077887ad6
Takafumi Tsuzuki <apumk6ke@ezweb.ne.jp>
2022-05-06 22:40:10 +0900

    色々修正

--- app/vol.1.7/function.cpp
+++ app/vol.1.7/function.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
 
+// 関数
+// 引数 x,y
+// 引数 intとint
+// int f()なので int型の値を返す必要がある
 int f(int x, int y)
 {
-    return 2 * x + y;
+    return 2 * x + y; // intを返却
 }
 
 int main()
 {
-    cout << "f(1,2) = " << f(1, 2) << endl;
-    cout << "f(182,144) = " << f(182, 144) << endl;
+    cout << "f(1,2) = " << f(1, 2) << endl;         // 関数呼び出し
+    cout << "f(182,144) = " << f(182, 144) << endl; // 関数呼び出し
 
     return 0;
 }
--- app/vol.1.8/void.cpp
+++ app/vol.1.8/void.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 using namespace std;
 
+// 関数名
 void hello()
 {
     cout << "hello world" << endl;
+
+    // 返り値を記述する必要はない
 }
 
+// 関数名
+// 引数を取ることができる
 void number(int a)
 {
     cout << "a = " << a << endl;
@@ -13,9 +18,9 @@ void number(int a)
 
 int main()
 {
+    hello(); // 同一ファイル内なのでそのまま呼び出せる
     hello();
-    hello();
-    number(1);
+    number(1); // 引数を取っていてもそのまま呼び出せる
     number(2);
 
     return 0;
