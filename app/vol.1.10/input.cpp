d9b383b67a23138b88184af20979af5eabfe73f9
Takafumi Tsuzuki <apumk6ke@ezweb.ne.jp>
2022-05-08 22:40:58 +0900

    vol.1.10

---
+++ app/vol.1.10/input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 0;
+
+    // 値を入力
+    cout << "数値を入力 >> ";
+    cin >> a;
+
+    // 入力値の表
+    cout << "a = " << a << endl;
+    cout << "a*3 = " << a * 3 << endl;
+
+    return 0;
+}
