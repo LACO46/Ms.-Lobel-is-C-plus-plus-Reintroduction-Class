9752e42af39f32664d905586a88cc9bfc39b0c85
Takafumi Tsuzuki <apumk6ke@ezweb.ne.jp>
2022-05-29 21:36:25 +0900

    vol.1.18.3

---
+++ app/vol.1.18.3/doWhile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+
+    // do{...}while{,,,}の
+    //   ,,,が一致しなくても
+    //   ...は１回は実行される
+    do
+    {
+        cout << i << endl;
+    } while (i != 0);
+
+    return 0;
+}
